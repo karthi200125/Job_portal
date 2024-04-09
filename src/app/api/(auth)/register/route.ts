4ca985fdb7ce0eb36afdd0489f9fa276698721c2
karthi200125 <skarthikeyan25052001@gmail.com>
2024-04-09 16:32:52 +0530

    .

--- src/app/api/(auth)/register/route.ts
+++
@@ -1,9 +0,0 @@
-import db from "@/utils/DbConnect";
-import { NextResponse } from "next/server";
-import bcrypt from 'bcryptjs'
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-    console.log(req.body); 
-}
-
