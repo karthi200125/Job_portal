e05c215be5429fd0afe073f1b62f2c2c2a530486
karthi200125 <skarthikeyan25052001@gmail.com>
2024-04-09 17:21:05 +0530

    add cors

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export async function POST(request: any) {
-    const { username, email, password, role } = await request.json()    
+export async function POST(request: NextRequest) {
+    const { username, email, password, role } = await request.json()
     console.log(username, email, password, role)
 
     return NextResponse.json("registed sucess")
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const origin = request.headers.get('origin');    
+    const headers: any = {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '86400',
+    };
+    if (request.method === 'OPTIONS') {        
+        return new Response(null, { headers });
+    } else {
+        return NextResponse.next();
+    }
+}
+
+export const config = {
+    matcher: '/about/:path*',
+};
